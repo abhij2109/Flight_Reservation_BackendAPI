5fb40479afc8d31864a0dbf85d4e3daba1367bed
abhij2109 <abhij2116@gmail.com>
2023-12-31 18:40:43 +0530

    Changed the Name of the files.

--- src/main/java/com/abhi/flightservices/FlightServicesApplication.java
+++ src/main/java/com/abhi/flightservices/FlightServicesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FlightservicesApplication {
+public class FlightServicesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FlightservicesApplication.class, args);
+		SpringApplication.run(FlightServicesApplication.class, args);
 	}
 
 }
